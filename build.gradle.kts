3ab435f7046211ccd6c279785eec0475a2ebbc6d
gihyeon6394 <gihyeon6394@gmail.com>
2024-07-02 12:30:59 +0900

    queryDSL을 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val springBootVersion: String by extra { "3.2.5" }
 val jacksonVersion: String by extra { "2.17.0" }
 val springCloudVersion: String by extra { "2023.0.2" }
+val querydslVersion: String by extra { "5.1.0" }
 
 
 plugins {
@@ -13,6 +14,7 @@ plugins {
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
     kotlin("plugin.jpa") version "1.9.23"
+    kotlin("kapt") version "2.0.0"
 
 }
 
@@ -33,6 +35,8 @@ dependencyManagement {
 }
 
 dependencies {
+    kapt("groupId:artifactId:version")
+
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -46,6 +50,12 @@ dependencies {
     implementation("jakarta.persistence:jakarta.persistence-api")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.0")
+
+    implementation("com.querydsl:querydsl-core:${querydslVersion}")
+    implementation("com.querydsl:querydsl-jpa:${querydslVersion}")
+    implementation("com.querydsl:querydsl-sql:${querydslVersion}")
+    implementation("com.querydsl:querydsl-apt:${querydslVersion}")
+    kapt("com.querydsl:querydsl-apt:${querydslVersion}:jpa")
 
     implementation("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
     implementation("com.fasterxml.jackson.core:jackson-annotations:$jacksonVersion")
